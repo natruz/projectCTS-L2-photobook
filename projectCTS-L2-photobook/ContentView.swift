864139ac4fa411db513b88feda8a6109ba67119f
natruz <88256962+natruz@users.noreply.github.com>
2022-06-22 10:41:00 +0800

    Initial Commit

---
+++ projectCTS-L2-photobook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  projectCTS-L2-photobook
+//
+//  Created by NATALIE CHEN HUI REGINA RUZSICSK stu on 22/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ projectCTS-L2-photobook/projectCTS_L2_photobookApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectCTS_L2_photobookApp.swift
+//  projectCTS-L2-photobook
+//
+//  Created by NATALIE CHEN HUI REGINA RUZSICSK stu on 22/6/22.
+//
+
+import SwiftUI
+
+@main
+struct projectCTS_L2_photobookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
