1e3b00fdba14c86f93e84890e49c4cb8937836f4
natruz <88256962+natruz@users.noreply.github.com>
2022-06-22 10:50:28 +0800

    Adding start variables: images, descriptions and index

--- projectCTS-L2-photobook/ContentView.swift
+++ projectCTS-L2-photobook/ContentView.swift
@@ -8,6 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var photos = [
+        "ICT-L2-photo1",
+        "ICT-L2-photo2",
+        "ICT-L2-photo3",
+        "ICT-L2-photo4",
+        "ICT-L2-photo5",
+    ]
+    var descriptions = [
+        "Mountain with lots of trees",
+        "Mountain with no trees",
+        "Trees",
+        "Mountain with lots of trees and NO mist",
+        "Mountain with lots of trees and mist",
+    ]
+    var index = 0
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
